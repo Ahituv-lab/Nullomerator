f347c1a56ae80813866ca7db0761a2f5864d762b
rle1323 <reasterlin@pcd-98bp3xy-lt.ucsfmedicalcenter.org>
2021-03-02 16:49:35 -0800

    Updating ExtractMutationNullomers

--- ExtractMutationNullomers.py
+++ ExtractMutationNullomers.py
@@ -1,8 +1,8 @@
 import re,os,sys,glob
-import pybedtools
 from Bio import SeqIO
+from Bio.Seq import Seq 
 
-class NullomerScanner():
+class ExtractMutationNullomers():
 
     def __init__(self, genome_fasta = "hg38p13_primary.fa", nullomer_file = "All_kmer_words_1_15nt_occurrences_genome_hg38", nullomer_len = 0):
         self.nullomer_set = None
@@ -102,9 +102,9 @@ class NullomerScanner():
         mutation_data = self._mutation_reader(mutation_file)
         print("Mutation read-in complete, starting mutation scan")
         nullomer_mutations = []
-        count = 0
         for item in mutation_data:
             chrom, pos, ref, alt = item[chr_col], int(item[pos_col]), item[ref_col], item[alt_col]
+            cancer_type, patient_id = item[0], item[1]
             chrom_key = "chr" + chrom
             ref_len = len(ref)
             alt_len = len(alt)
@@ -120,8 +120,9 @@ class NullomerScanner():
             elif ref_len > 1:
                 right_flank = self.genome_dict[chrom_key][pos + ref_len - 1: pos + ref_len + self.max_null_length - 2].upper()
 
-            # generate the mutated sequence
+            # generate the mutated sequence, and its reverse complement
             variant_motif = left_flank + alt + right_flank
+            variant_rc = str(Seq(variant_motif).reverse_complement())
             motif_len = len(variant_motif)
 
             # search for nullomers in the new sequence
@@ -130,23 +131,54 @@ class NullomerScanner():
                 for seq_len in range(1, self.max_null_length + 1):
                     for i in range(0, motif_len-seq_len+1):
                         subsequence = variant_motif[i:i+seq_len]
+                        rc_subsequence = variant_rc[i:i+seq_len]
+
                         if subsequence in self.nullomer_set:
                             found_nullomers.append(subsequence)
+                        if rc_subsequence in self.nullomer_set:
+                            found_nullomers.append(rc_subsequence)
+                        
             # if the nullomer length is fixed, the computation is slightly different
             else:
                 for i in range(0, motif_len-self.nullomer_len):
                     subsequence = variant_motif[i:i+self.nullomer_len]
+                    rc_subsequence = variant_rc[i:i+self.nullomer_len]
                     if subsequence in self.nullomer_set:
                         found_nullomers.append(subsequence)
+                    if rc_subsequence in self.nullomer_set:
+                        found_nullomers.append(rc_subsequence)
 
             # if at least one new nullomer found, put it with the mutation and append it to a *special* list
             if len(found_nullomers) != 0:
-                nullomer_mutations.append([chrom, pos, ref, alt, found_nullomers])
+                # get length of longest nullomer
+                max_len = 0
+                for null in found_nullomers:
+                    length = len(null)
+                    if  length > max_len:
+                        max_len = length
+                
+                # each line should be of format: chrom, start, end, cancer type patient id REF ALT seq nullomers
+                nullomer_mutations.append((chrom, pos-max_len, pos+max_len, cancer_type, patient_id, ref, alt, variant_motif, found_nullomers))
 
         return nullomer_mutations
 
 
-# Example usage
-example_scanner = NullomerScanner(genome_fasta="../nullomer_scanner/genome_files/hg38p13_primary.fa", nullomer_file="../nullomer_scanner/All_kmer_words_1_15nt_occurrences_genome_hg38", nullomer_len=0)
-mutations = example_scanner.scan_mutations("../nullomer_scanner/mutations/denovo_indels.txt", chr_col=5, pos_col=6, ref_col=7, alt_col=8)
-print(mutations)
+
+# Input filename into the object for use
+if __name__ == "__main__":
+    # change the genome_fasta path to your genome file
+    example_scanner = ExtractMutationNullomers(genome_fasta="genome_files/chr1_hg37.fa", nullomer_file="All_kmer_words_1_15nt_occurrences_genome_hg38", nullomer_len=0)
+    # change the path to your mutation file
+    mutations = example_scanner.scan_mutations("genome_files/mutation_example.txt", chr_col=2, pos_col=3, ref_col=4, alt_col=5)
+    
+    # write the output file
+    with open("mutation_nullomers.txt", "w") as mutation_file:
+        for mutation in mutations:
+            for item in mutation:
+                if isinstance(item, list):
+                    for null in item:
+                        mutation_file.write(null + ",")
+                else:
+                    mutation_file.write(str(item) + "\t")
+            mutation_file.write("\n")
+            
