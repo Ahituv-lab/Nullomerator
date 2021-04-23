2709409daf76dd7d1b873afaa8c5e9a04c1f9606
Ryder Easterlin <ryder.easterlin@ucsf.edu>
2021-04-23 13:03:45 -0700

    handling edge case of fnv

--- ExtractMutationNullomers.py
+++ ExtractMutationNullomers.py
@@ -28,7 +28,9 @@ class ExtractMutationNullomers():
         """
         genome_dict = {}
         for section in SeqIO.parse(genome_fasta, "fasta"):
-            genome_dict[str(section.description)] = str(section.seq).upper()
+            section_description = str(section.description).split()
+            section_header = section_description[0]
+            genome_dict[section_header] = str(section.seq).upper()
         self.genome_dict = genome_dict
         print("Genome read-in complete")
         
@@ -64,7 +66,7 @@ class ExtractMutationNullomers():
             self.nullomer_set = nullomer_set
             print("Nullomer read-in complete")
 
-    def _mutation_reader(self, mutation_file):
+    def _mutation_reader(self, mutation_file, comments):
         """ 
         reads in a vcf file holding mutation data and returns each line as a list with each word in the line as an element
         """
@@ -72,13 +74,15 @@ class ExtractMutationNullomers():
         file_lines = datafile.readlines()
         datafile.close()
         mutation_data = []
-        for i in file_lines[1:]:
-            items = [i.strip().split('\t')]
-            mutation_data+=[i.strip().split('\t')]     
+        for i in file_lines:
+            # if this isnt a comment line, break it up and save it because it contains mutation data
+            if i[0] != comments:
+                mutation_data+=[i.strip().split('\t')]
+
         return mutation_data
 
 
-    def scan_mutations(self, mutation_file, chr_col=0, pos_col=1, ref_col=3, alt_col=4):
+    def scan_mutations(self, mutation_file, chr_col=0, pos_col=1, ref_col=3, alt_col=4, comments="#"):
         """
         Takes in a vcf file of mutations, and looks for whether the create a nullomer in the genome
 
@@ -98,16 +102,20 @@ class ExtractMutationNullomers():
             nullomer_mutations::list
                 A list of variants that create nullomers. For each variant, the chr, pos, reference allele, alternative allele, and nullomers it creates are returned.
             """
-        mutation_data = self._mutation_reader(mutation_file)
+        mutation_data = self._mutation_reader(mutation_file, comments="#")
         print("Mutation read-in complete, starting mutation scan")
         nullomer_mutations = []
         for item in mutation_data:
             chrom, pos, ref, alt = item[chr_col], int(item[pos_col]), item[ref_col].upper(), item[alt_col].upper()
-            cancer_type, patient_id = item[0], item[1]
-            chrom_key = "chr" + chrom
+            meta_data = [item[i] for i in range(len(item)) if i not in [chr_col, pos_col, ref_col, alt_col]]
+            try:
+                chrom_int = int(chrom)
+                chrom_key = "chr" + chrom
+            except: 
+                chrom_key = chrom
+                
             ref_len = len(ref)
             alt_len = len(alt)
-
             
             # the bases leading up to the mutation site will always be the same, so no conditionals needed
             left_flank = self.genome_dict[chrom_key][pos - self.max_null_length : pos - 1]
@@ -121,6 +129,9 @@ class ExtractMutationNullomers():
                 right_flank = self.genome_dict[chrom_key][pos + ref_len - 1: pos + ref_len + self.max_null_length - 2]
 
             # generate the mutated sequence, and its reverse complement
+            if alt == "." or alt == "-":
+            	alt = ""
+            	
             variant_motif = left_flank + alt + right_flank
             variant_rc = str(Seq(variant_motif).reverse_complement())
             motif_len = len(variant_motif)
@@ -158,11 +169,29 @@ class ExtractMutationNullomers():
                     if length > max_len:
                         max_len = length
                 # each line should be of format: chrom, start, end, cancer type patient id REF ALT seq nullomers
-                nullomer_mutations.append((chrom, pos-max_len, pos+max_len, cancer_type, patient_id, ref, alt, variant_motif, found_nullomers))
+                nullomer_mutations.append((chrom, pos-max_len, pos+max_len, ref, alt, variant_motif, found_nullomers))
             # if no nullomers are found for that variant, just put "None" in the found_nullomers column
             else:
-                nullomer_mutations.append((chrom, pos-self.max_null_length, pos+self.max_null_length, cancer_type, patient_id, ref, alt, variant_motif, "None"))
+                nullomer_mutations.append((chrom, pos-self.max_null_length, pos+self.max_null_length, ref, alt, variant_motif, "None"))
         return nullomer_mutations
+    
+    @staticmethod
+    def write_output(output_filepath, mutations):
+        """
+        Writes an output file given a list of lines, where each item in a line is a single item in a list. 
+        """
+        with open(output_filepath, "w") as mutation_file:
+            for mutation in mutations:
+                for item in mutation:
+                    if isinstance(item, list):
+                        for null in item:
+                            mutation_file.write(null + ",")
+                    elif item == "None":
+                        mutation_file.write(item)
+                    else:
+                        mutation_file.write(str(item) + "\t")
+                mutation_file.write("\n")
+
 
 
 
@@ -170,19 +199,13 @@ class ExtractMutationNullomers():
 if __name__ == "__main__":
     # change the genome_fasta path to your genome file
     example_scanner = ExtractMutationNullomers(genome_fasta="genome_files/chr1_hg37.fa", nullomer_file="nullomer+kmer_files/nullomers_chr1_hg37.txt", nullomer_len=0)
-    # change the path to your mutation file
+
+    # change the path to your mutation file. The positional arguments after the path argument mark the columns in which each piece of relevant data lies.
+    # for example, looking for mutations stored in a vcf file would look like: 
+    #                                           example_scanner.scan_mutations("path_to_vcf", chr_col=0, pos_col=1, ref_col=3, alt_col=4)
     mutations = example_scanner.scan_mutations("genome_files/mutation_example.txt", chr_col=2, pos_col=3, ref_col=4, alt_col=5)
     
-    # write the output file
-    with open("mutation_nullomers.txt", "w") as mutation_file:
-        for mutation in mutations:
-            for item in mutation:
-                if isinstance(item, list):
-                    for null in item:
-                        mutation_file.write(null + ",")
-                elif item == "None":
-                    mutation_file.write(item)
-                else:
-                    mutation_file.write(str(item) + "\t")
-            mutation_file.write("\n")
+    # write the output file. if you are using this, just change the output filepath to your desired location 
+    ExtractMutationNullomers.write_output("mutation_nullomers.tsv",mutations)
+    
             
